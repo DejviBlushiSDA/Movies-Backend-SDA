0e30c9dd6a84ca1e65ba2094b1ca21370bde7e5a
Dejvi-Blushi <dejviblushi5@gmail.com>
2025-02-08 11:08:47 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+// import newsRoutes from './routes/news.js';
+import moviesRoutes from './routes/movies.js';
+import authRoutes from './routes/router.js';
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// CORS headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+app.use(authRoutes);
+app.use('/movies', moviesRoutes);
+
+// Error handling middleware
+app.use((error, req, res, next) => {
+  const status = error.status || 500;
+  const message = error.message || 'Something went wrong.';
+  res.status(status).json({ message: message });
+});
+
+// Start server
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
